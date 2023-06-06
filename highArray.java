8f5bd1a65dd16ecc6cc80d2a486fbc9082638f43
Tiana <tiana.aakins95@gmail.com>
2023-06-06 02:51:14 -0500

    added construtor and find() method to HighArray class

--- highArray.java
+++ highArray.java
@@ -1,4 +1,22 @@
 class HighArray {
     private long[] a;
     private int nElems;
+
+    public HighArray(int max){
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public boolean find(long searchKey){
+        int j;
+        for(j=0; j<nElems; j++)
+            if (a[j] == searchKey)
+            break;
+        if(j == nElems)
+            return false;
+        else
+            return true;
+    }
+
+
 }
